92e95b398d17481eea3e665120dce7fabe71df10
vonB22 <balanavonlloyd22@gmail.com>
2025-09-15 14:45:56 -0600

    Remove duplicate routes

--- routes/api.php
+++ routes/api.php
@@ -6,8 +6,3 @@ use Illuminate\Support\Facades\Route;
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
-
-Route::post('/register', [RegisteredUserController::class, 'store']);
-Route::post('/login', [AuthenticatedSessionController::class, 'store']);
-Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])
-    ->middleware('auth:sanctum');
