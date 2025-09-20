7423858b9d46d73d4740329e6e964351397abe23
vonB22 <balanavonlloyd22@gmail.com>
2025-09-20 17:41:18 -0600

    remove unused file

--- resources/views/test-login.blade.php
+++
@@ -1,6 +0,0 @@
-<form method="POST" action="/login">
-    @csrf
-    <input type="email" name="email" placeholder="Email">
-    <input type="password" name="password" placeholder="Password">
-    <button type="submit">Login</button>
-</form>
