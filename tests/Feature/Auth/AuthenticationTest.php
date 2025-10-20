7babd039c556c620959c96488420db06a6170c29
vonB22 <balanavonlloyd22@gmail.com>
2025-10-20 10:17:47 -0600

    updated

--- tests/Feature/Auth/AuthenticationTest.php
+++ tests/Feature/Auth/AuthenticationTest.php
@@ -12,7 +12,9 @@ class AuthenticationTest extends TestCase
 
     public function test_users_can_authenticate_using_the_login_screen(): void
     {
-        $user = User::factory()->create();
+        $user = User::factory()->create([
+            'password' => bcrypt('password'),
+        ]);
 
         $response = $this->post('/login', [
             'email' => $user->email,
